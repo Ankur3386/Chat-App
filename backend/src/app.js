b1afb461d6cfcaae35d6626fdba12d079517fae9
Ankur3386 <ankurdeep03386@gmail.com>
2025-04-02 01:32:41 +0530

    models

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,2 +1,6 @@
 import express from 'express'
-const app =express()
+import dotenv from 'dotenv'
+
+const app =express();
+dotenv.config();
+export default app
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 
 import './App.css'
 import Home from './components/Home'
+import { Route, Routes } from 'react-router-dom'
+import ChatPage from './components/ChatPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    <Home/>
-    </>
+    <Routes>
+    <Route path='/' Component={Home}/>
+    <Route path='/chats' Component={ChatPage}/>
+    </Routes>
   )
 }
 
